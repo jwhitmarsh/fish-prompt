69e7e73267904d976868fad854705f538e8f2490
James Whitmarsh <james@liftedcare.com>
2023-11-01 17:04:49 +0000

    Move prompt onto new line, remove right prompt
    This makes copy/pasting commands _much_ easier

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
 function fish_right_prompt
-  set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
-  date "+%H:%M:%S"
-  set_color normal
+    # i don't want a right prompt
+    # set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
+    # date "+%H:%M:%S"
+    # set_color normal
 end
